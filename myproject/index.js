057b02c69771b3d24d104040f9a2d0ace2efb9f8
DESKTOP-PJ3UT06\JangHyunJin <winckey0@naver.com>
2023-03-16 01:52:18 +0900

    ec2 test

--- myproject/index.js
+++ myproject/index.js
@@ -1,8 +1,11 @@
-const express = require('express') //express를 설치했기 때문에 가져올 수 있다.
+const express = require('express')
 const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(8080)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
